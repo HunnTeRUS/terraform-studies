1aa0d484b72aec05035a07027b4cbb25ec2ee77d
hunter <hunter@pop-os.localdomain>
2023-09-25 20:41:59 -0300

    Resolving conflict

---
+++ terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.17"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+  }
+}
