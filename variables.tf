68ad8a7f5b0cf6a549ddc8bb6a726779d1ae793f
hunter <hunter@pop-os.localdomain>
2023-09-25 20:41:59 -0300

    Resolving conflict

---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment for this project"
+  type        = string
+
+  default = "dev"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
+}
+
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
+
+variable "variables_sub_cidr" {
+  description = "CIDR Block for the variables subnet"
+  type        = string
+  default     = "10.0.202.0/24"
+}
+
+variable "variables_sub_az" {
+  description = "Availability Zone used variables subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "variables_sub_auto_ip" {
+  description = "Set automatic ip assigment for variables subnet"
+  type        = bool
+  default     = true
+}
